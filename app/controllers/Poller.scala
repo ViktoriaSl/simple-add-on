c4a84ba4c20d890ae47706cdce071d8bd8b251c5
vselivanova <viktoriia.selivanova@webtech.in.ua>
2016-12-16 17:07:55 +0200

    update ViewerIssueValue

--- app/controllers/Poller.scala
+++ app/controllers/Poller.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import scala.concurrent.Future
 import com.atlassian.connect.playscala.PlayAcConfigured
 import com.atlassian.connect.playscala.auth.Token
 import com.atlassian.connect.playscala.controllers.ActionJwtValidator
@@ -7,11 +8,23 @@ import com.atlassian.connect.playscala.store.DefaultDbConfiguration
 import play.api.mvc.{Action, Controller}
 import service.ViewerIssueValue
 import utils.Constants.VALIDATED_SUCCESSFULLY
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import views._
+import models._
+import scala.collection.mutable.ArrayBuffer
+import play.api.libs.json._
+import play.api.libs.json.Json
+import play.api.libs.json.Json._
+import java.util.Calendar
 
 object Poller extends Controller with ViewerIssueValue with ActionJwtValidator with DefaultDbConfiguration
   with PlayAcConfigured {
   
-  def index() = Action { implicit request =>
+  def index() =  Action { implicit request =>
     jwtValidated { implicit token: Token =>
       val hostId = token.acHost.key.value
       (for {
--- test/ApplicationFunSuite.scala
+++ test/ApplicationFunSuite.scala
@@ -1,18 +1,18 @@
 import com.atlassian.connect.playscala.AcConfig
 import com.atlassian.connect.playscala.model.AcHostModelStore
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.{FunSuite, Matchers, PrivateMethodTester}
+import play.api.libs.json.Json
 import service.ViewerIssueValue
+import play.api.mvc._
 
-class ApplicationFunSuite extends FunSuite with Matchers with ViewerIssueValue {
-  
+class ApplicationFunSuite extends FunSuite with Matchers with ViewerIssueValue{
   test("with positive num should return success") {
     validatePositiveField(1) shouldEqual "Correct number! "
   }
   
-  test("with positive num should return success") {
+  test("with positive num should return fail") {
     validatePositiveField(-1) shouldEqual "Validation wasn't success. Please set positive number !"
   }
-  
   override implicit def acConfig: AcConfig = ???
   override def acHostModelStore: AcHostModelStore = ???
 }
